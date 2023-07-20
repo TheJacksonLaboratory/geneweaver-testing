0d440eef1afe7536bb56d9dec9fc86713703efeb
Alexander Berger <alexander.berger@jax.org>
2023-07-19 21:19:39 -0400

    Finishing test modifications by adding conditionals about if package is a tools package

--- src/geneweaver/testing/fixtures/package.py
+++ src/geneweaver/testing/fixtures/package.py
@@ -13,6 +13,7 @@ __all__ = [
     "pyproject_toml_contents",
     "package_name_from_pyproject",
     "package_submodule_name",
+    "is_tool_package",
 ]
 
 
@@ -73,3 +74,9 @@ def package_submodule_name(package_name_from_pyproject: Optional[str]) -> Option
         if package_name_from_pyproject
         else None
     )
+
+
+@pytest.fixture(scope="session")
+def is_tool_package(project_root: Optional[str]) -> bool:
+    """Return True if the package is a tool package."""
+    return (project_root / "src" / "geneweaver" / "tools").is_dir()
